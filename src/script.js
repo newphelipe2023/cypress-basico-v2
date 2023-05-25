6745480833b7d360fc68d19ca99736a20be84d89
newphelipe2023 <pheliperochasilva@gmail.com>
2023-05-24 21:52:14 -0300

    Break the app on purpose

--- src/script.js
+++ src/script.js
@@ -35,8 +35,8 @@ document.querySelector('button[type="submit"]')
     if (isPhoneRequired && !phoneField.value) {
       return showAndHideErrorMessage()
     }
-    //if (!emailField.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
-      //return showAndHideErrorMessage()
+    if (!emailField.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
+      return showAndHideErrorMessage()
     }
     firstNameField.value = ''
     lastNameField.value = ''
